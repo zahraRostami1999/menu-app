f41e9f1975ea33b1dbef88a1f347d1d0d9ef0cf6
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-03 12:51:52 +0330

    edit MenuPage

--- src/pages/menu/MenuPage.jsx
+++ src/pages/menu/MenuPage.jsx
@@ -1,17 +1,18 @@
-import styles from './MenuPage.module.css';
-import List from '../../components/list/List.jsx'
+import styles from "./MenuPage.module.css";
+import List from "../../components/list/List.jsx";
 
-
-function MenuPage(){
-    const foods = [
-        {id:1, name:"Pizza", amount:0, price:'5$'},
-        {id:2, name:"Pasta", amount:0, price:'3$'},
-        {id:3, name:"Fried chicken", amount:0, price:'4$'}
-    ]
-
-    return(
-        <List items={foods} category='Food'/>
-    );
+function MenuPage() {
+  const foods = [
+    { id: 1, name: "Pizza", amount: 0, price: "5$" },
+    { id: 2, name: "Pasta", amount: 0, price: "3$" },
+    { id: 3, name: "Fried chicken", amount: 0, price: "4$" },
+  ];
+  return (
+    <>
+      <List items={foods} category="Food" />
+      <List />
+    </>
+  );
 }
 
 export default MenuPage;
