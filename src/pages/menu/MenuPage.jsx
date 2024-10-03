cd82c9ea9dd202bed225aaca517372f45f6b8fe0
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-03 12:39:36 +0330

    edit MenuPage: import List component and define foods list and put them into List componnet props

---
+++ src/pages/menu/MenuPage.jsx
@@ -0,0 +1,17 @@
+import styles from './MenuPage.module.css';
+import List from '../../components/list/List.jsx'
+
+
+function MenuPage(){
+    const foods = [
+        {id:1, name:"Pizza", amount:0, price:'5$'},
+        {id:2, name:"Pasta", amount:0, price:'3$'},
+        {id:3, name:"Fried chicken", amount:0, price:'4$'}
+    ]
+
+    return(
+        <List items={foods} category='Food'/>
+    );
+}
+
+export default MenuPage;
