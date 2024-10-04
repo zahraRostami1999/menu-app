63a3af777c376408cbabf983bf6d020fc6952058
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-04 10:59:54 +0330

    edit MenuPage: add drinks and appetizers list

--- src/pages/menu/MenuPage.jsx
+++ src/pages/menu/MenuPage.jsx
@@ -3,14 +3,26 @@ import List from "../../components/list/List.jsx";
 
 function MenuPage() {
   const foods = [
-    { id: 1, name: "Pizza", amount: 0, price: "5$" },
-    { id: 2, name: "Pasta", amount: 0, price: "3$" },
-    { id: 3, name: "Fried chicken", amount: 0, price: "4$" }
+    { id: 1, name: "Pizza", amount: 0, price: "$9" },
+    { id: 2, name: "Pasta", amount: 0, price: "$13" },
+    { id: 3, name: "Fried chicken", amount: 0, price: "$10" }
   ];
+  const drinks = [
+    { id: 1, name: "Beer", amount: 0, price: "$3" },
+    { id: 2, name: "Pepsi", amount: 0, price: "$1" },
+    { id: 3, name: "Coca-Cola", amount: 0, price: "$1" }
+  ];
+  const appetizers  = [
+    { id: 1, name: "French Fries", amount: 0, price: "$5" },
+    { id: 2, name: "onion rings", amount: 0, price: "$6" },
+    { id: 3, name: "Mozzarella Sticks", amount: 0, price: "$8" }
+  ];
+
   return (
     <>
-      <List items={foods} category="Food" />
-      {/* <List /> */}
+      {foods.length >0 && <List items={foods} category="Food" />}
+      {drinks.length >0 && <List items={drinks} category="Drinks" />}
+      {appetizers.length >0 && <List items={appetizers} category="Appetizers" />}
     </>
   );
 }
