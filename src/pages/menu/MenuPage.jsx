095f75e8a8fa43f7e3342898da93600fffbaa257
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-04 10:08:13 +0330

    edit MenuPage: comment the secaund call List component

--- src/pages/menu/MenuPage.jsx
+++ src/pages/menu/MenuPage.jsx
@@ -5,12 +5,12 @@ function MenuPage() {
   const foods = [
     { id: 1, name: "Pizza", amount: 0, price: "5$" },
     { id: 2, name: "Pasta", amount: 0, price: "3$" },
-    { id: 3, name: "Fried chicken", amount: 0, price: "4$" },
+    { id: 3, name: "Fried chicken", amount: 0, price: "4$" }
   ];
   return (
     <>
       <List items={foods} category="Food" />
-      <List />
+      {/* <List /> */}
     </>
   );
 }
