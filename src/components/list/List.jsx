a352efb7132d8b71ff7b8e03fa6876e7b55f5c1e
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-04 11:00:47 +0330

    edit List component: add default props

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,27 +1,40 @@
 import PropTypes from "prop-types";
 
-function List(props){
+function List(props) {
+  const Listitems = props.items;
+  const category = props.category;
 
-    const Listitems = props.items;
-    const category = props.category;
+  const foodsList = Listitems.map((item) => (
+    <li key={item.id}>
+      {item.name}: {item.price}
+    </li>
+  ));
 
-    const foodsList = Listitems.map((item) => (<li key={item.id}>{item.name}: {item.price}</li>));
-
-    return(
-        <>
-        <div>
-            <h3>{category}</h3>
-            <ul>{foodsList}</ul>
-        </div>
-        </>
-    );
+  return (
+    <>
+      <div>
+        <h3>{category}</h3>
+        <ul>{foodsList}</ul>
+      </div>
+    </>
+  );
 }
 
 List.propTypes = {
-    id: PropTypes.number,
-    name: PropTypes.string,
-    amount: PropTypes.number,
-    price: PropTypes.string
-  };
+  category: PropTypes.string,
+  Listitems: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+      price: PropTypes.string,
+      amount: PropTypes.number,
+    })
+  ),
+};
+
+List.defaultProp = {
+  Listitems: [],
+  category: "CategoryName",
+};
 
 export default List;
