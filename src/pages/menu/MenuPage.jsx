a03668f760879e66896be4e2cd8ede5377f728af
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-04 12:13:43 +0330

    edit MenuPage: import background image and set inline style for background image div

--- src/pages/menu/MenuPage.jsx
+++ src/pages/menu/MenuPage.jsx
@@ -1,28 +1,42 @@
 import styles from "./MenuPage.module.css";
 import List from "../../components/list/List.jsx";
+import background from "../../assets/back2.jpg";
 
 function MenuPage() {
   const foods = [
     { id: 1, name: "Pizza", amount: 0, price: "$9" },
     { id: 2, name: "Pasta", amount: 0, price: "$13" },
-    { id: 3, name: "Fried chicken", amount: 0, price: "$10" }
+    { id: 3, name: "Fried chicken", amount: 0, price: "$10" },
   ];
   const drinks = [
     { id: 1, name: "Beer", amount: 0, price: "$3" },
     { id: 2, name: "Pepsi", amount: 0, price: "$1" },
-    { id: 3, name: "Coca-Cola", amount: 0, price: "$1" }
+    { id: 3, name: "Coca-Cola", amount: 0, price: "$1" },
   ];
-  const appetizers  = [
+  const appetizers = [
     { id: 1, name: "French Fries", amount: 0, price: "$5" },
     { id: 2, name: "onion rings", amount: 0, price: "$6" },
-    { id: 3, name: "Mozzarella Sticks", amount: 0, price: "$8" }
+    { id: 3, name: "Mozzarella Sticks", amount: 0, price: "$8" },
   ];
+
+  const style = {
+    backgroundImage: `url(${background})`,
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    height: "100vh",
+    opacity: "0.9"
+  };
 
   return (
     <>
-      {foods.length >0 && <List items={foods} category="Food" />}
-      {drinks.length >0 && <List items={drinks} category="Drinks" />}
-      {appetizers.length >0 && <List items={appetizers} category="Appetizers" />}
+      <div style={style}>
+        <div className={styles.list}>
+          {foods.length > 0 && <List items={foods} category="Food" />}
+          {drinks.length > 0 && <List items={drinks} category="Drinks" />}
+        {appetizers.length > 0 && <List items={appetizers} category="Appetizers" />}
+        </div>
+      </div>
     </>
   );
 }
